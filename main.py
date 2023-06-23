71451fd7109ed57980956f133c3fadae575b5729
caseyrwebb <caseyrwebb@gmail.com>
2023-06-22 22:20:12 -0500

    Added word wrappin

--- main.py
+++ main.py
@@ -14,24 +14,40 @@ from letter_class import Letter
 
 logging.basicConfig(level=logging.ERROR)
 
-size = os.get_terminal_size()
-
 
 def store_sentence(words: List[str], letter_representation: dict[str, list[list[str]]]):
+    terminal_width = os.get_terminal_size().columns
     sentence = []
+    word_group = []
+    current_width = 0
     for word in words:
         word_representation = []
+        word_width = 0
         for letter in word:
-            word_representation.append(letter_representation[letter.upper()])
-        sentence.append(word_representation)
+            single_letter_representation = letter_representation[letter.upper()]
+            word_representation.append(single_letter_representation)
+            letter_width = (
+                max(len("".join(line)) for line in single_letter_representation) + 1
+            )
+            word_width += letter_width
+        if current_width + word_width + len(word_group) * 4 > terminal_width:
+            sentence.append(word_group)
+            word_group = [word_representation]
+            current_width = word_width
+        else:
+            word_group.append(word_representation)
+            current_width += word_width
+    if word_group:
+        sentence.append(word_group)
     return sentence
 
 
-def print_sentence(sentence):
+def print_sentence(word_group):
     for i in range(5):
-        for word_representation in sentence:
+        for word_representation in word_group:
             for letter_representation in word_representation:
-                print("".join(letter_representation[i]), end="")
+                printed_string = "".join(letter_representation[i])
+                print("\033[33m" + printed_string, end="")
             print("    ", end="")
         print()
 
@@ -40,8 +56,9 @@ def main(words: str, letter_representation: dict[str, list[list[str]]]):
     words = words.split()
 
     sentence = store_sentence(words, letter_representation)
-    print_sentence(sentence)
-    print(size)
+    for word_group in sentence:
+        print_sentence(word_group)
+        print()
 
 
 if __name__ == "__main__":
